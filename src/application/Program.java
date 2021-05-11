f0454dcc2d8e93ac52ff3161f9053a6bc2576286
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-05-11 05:08:26 -0300

    Create find by id

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,11 +10,11 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(seller);
+		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
-		SellerDao seleerdao = DaoFactory.createSellerDao();
+		Seller seller = sellerdao.findById(3);
+		
+		System.out.println(seller);
 
 	}
 
